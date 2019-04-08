4fb8ebf9ef9d53dcd208d07d9044562e183fc275
uljot <joasia.ulman@gmail.com>
2019-04-08 19:37:38 +0200

    added navigator to alert

--- www/js/pgb.js
+++ www/js/pgb.js
@@ -14,14 +14,14 @@ var watchID = navigator.accelerometer.watchAcceleration(accelerometerSuccess,
 	accelerometerOptions);
 
 function onSuccess(acceleration) {
-    alert('Acceleration X: ' + acceleration.x + '\n' +
+    navigator.notification.alert('Acceleration X: ' + acceleration.x + '\n' +
           'Acceleration Y: ' + acceleration.y + '\n' +
           'Acceleration Z: ' + acceleration.z + '\n' +
           'Timestamp: '      + acceleration.timestamp + '\n');
 }
 
 function onError() {
-    alert('onError!');
+    navigator.notification.alert('onError!');
 }
 
 var options = { frequency: 3000 };  // Update every 3 seconds
