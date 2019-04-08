4af4d8fe8e347923706e6223514362fdc614ecd4
uljot <joasia.ulman@gmail.com>
2019-04-08 19:33:27 +0200

    added accelerometer plugin

--- www/js/pgb.js
+++ www/js/pgb.js
@@ -6,7 +6,27 @@ function init() {
 
 function onDeviceReady() {
 	navigator.notification.beep(1);
+	console.log(navigator.accelerometer);
 }
+
+var watchID = navigator.accelerometer.watchAcceleration(accelerometerSuccess,
+	accelerometerError,
+	accelerometerOptions);
+
+function onSuccess(acceleration) {
+    alert('Acceleration X: ' + acceleration.x + '\n' +
+          'Acceleration Y: ' + acceleration.y + '\n' +
+          'Acceleration Z: ' + acceleration.z + '\n' +
+          'Timestamp: '      + acceleration.timestamp + '\n');
+}
+
+function onError() {
+    alert('onError!');
+}
+
+var options = { frequency: 3000 };  // Update every 3 seconds
+
+var watchID = navigator.accelerometer.watchAcceleration(onSuccess, onError, options);
 
 function deviceInfo() {
 
